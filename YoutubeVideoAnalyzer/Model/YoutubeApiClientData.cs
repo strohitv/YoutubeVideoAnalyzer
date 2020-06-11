bfbd90ef05fd3821a1cddcbf921de95f4b713e1f
marco <goethe@marco-fuchs.de>
2020-06-11 13:52:42 +0200

    fixed untracked files

--- YoutubeVideoAnalyzer/Model/YoutubeApiClientData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace YoutubeVideoAnalyser.Model
-{
-	public static class YoutubeApiClientData
-	{
-		public static string ClientId => "Your client id here";
-		public static string ClientSecret => "Your client secret here";
-	}
-}
